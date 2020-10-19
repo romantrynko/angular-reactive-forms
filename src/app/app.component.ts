5fded7f53d9a8a561db8c268494cf6345d0ba54e
romantrynko <trynkoroman25@gmail.com>
2020-10-19 20:22:41 +0300

    reactive forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,42 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  form: FormGroup;
+  id: FormControl = new FormControl('', Validators.required);
+  username: FormControl = new FormControl(
+    '',
+    [Validators.required,
+      Validators.maxLength(3),
+      this.rudeWordsValidate]
+    );
+
+  constructor() {
+    this.form = new FormGroup(
+      {
+        id: this.id,
+        username: this.username
+      },
+    );
+  }
+
+  // tslint:disable-next-line: typedef
+  rudeWordsValidate(controls: AbstractControl) {
+    console.log(controls.value);
+    if (controls.value.includes('durak')) {
+      return { rudeWord: true };
+    }
+    return null;
+  }
+
+  action(): void {
+    console.log(this.id.value);
+    console.log(this.form);
+    console.log(this.id);
+    console.log(this.username);
+
+  }
+
+
+
 }
